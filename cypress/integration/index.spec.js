ed247695a11f15cfa2852c0852444139d7c8f24b
Eirik Birkeland <birketrans@gmail.com>
2018-07-07 03:04:40 +0200

    enhance Cypress e2e tests

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,37 +1,41 @@
 describe('Translation Editor', () => {
+    const segments = '[data-e2e="segments"] input';
 
     it('should allow translating', () => {
         cy.visit('localhost:3000');
-        cy.get('[data-e2e="segments"] input').eq(0).type("Når man skriver et brev...");
-        cy.get('[data-e2e="segments"] input').eq(1).type("Vi burde være takknemlig for...");
-        cy.get('[data-e2e="segments"] input').eq(2).type("Det er tafatt å tenke at...");
+        cy.get(segments).eq(0).clear().type("Når man skriver et brev...");
+        cy.get(segments).eq(1).clear().type("Vi burde være takknemlig for...");
+        cy.get(segments).eq(2).clear().type("Det er tafatt å tenke at...");
     });
 
     it('should find/replace plain text', () => {
         cy.visit('localhost:3000');
-        cy.get('#find').type('klar');
-        cy.get('#replace').type('ikke klar!');  
+        cy.get('#find').type('man skriver');
+        cy.get('#replace').type('man leser');  
         cy.contains('Apply').click().trigger('blur');
-        // This won't work for input value apparently ... use textarea? contenteditable?
-        // cy.contains('Middagen er ikke klar!');
+
+        cy.get(segments).eq(0).should('have.value', 'Når man leser et brev...');
     });
+
     it('should find/replace case-insensitively (regex)', () => {
         cy.visit('localhost:3000');
-        cy.get('#find').type('BADET');
-        cy.get('#replace').type('badet din gamle sjokolade!');
+        cy.get('#find').type('NÅR MAN SKRIVER');
+        cy.get('#replace').type('Når man leser');
         cy.get('#regex').click();
         cy.get('#caseInsensitive').click();
         cy.contains('Apply').click().trigger('blur');
-        // cy.contains('Ha det på badet din gamle sjokolade');
+
+        cy.get(segments).eq(0).should('have.value', 'Når man leser et brev...');
     });
+
     it('should find/replace ALL (regex)', () => {
         cy.visit('localhost:3000');
-        cy.get('#find').type('\\w+');
-        cy.get('#replace').type('cyka blyat!');
+        cy.get('#find').type('\\S+');
+        cy.get('#replace').type('cyka blyat');
         cy.get('#regex').click();
         cy.get('#replaceAll').click();
-
         cy.contains('Apply').click().trigger('blur');
-        // cy.contains('Ha det på badet din gamle sjokolade');
+
+        cy.get(segments).eq(0).should('have.value', 'cyka blyat cyka blyat cyka blyat cyka blyat cyka blyat');
     });
 })
