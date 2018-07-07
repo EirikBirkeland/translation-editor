883f6be743ed2d967ec0ab7b067623d9ee5d323d
Eirik Birkeland <birketrans@gmail.com>
2018-07-07 02:07:39 +0200

    make e2e test pass (regex option was unaccounted for in the Redux migration)

--- src/components/SearchAndReplace.js
+++ src/components/SearchAndReplace.js
@@ -3,62 +3,75 @@ import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const SearchAndReplace = ({ searchReplace }) => {
-    let regex = false;
+class SearchAndReplace extends React.Component {
 
-    return (
-        <div>
-            <form onSubmit={(event) => {
-                event.preventDefault();
+    constructor(props) {
+        super();
+    }
 
-                const { find, replace, regex, replaceAll, caseInsensitive } = event.target.elements;
+    state = {
+        regex: false,
+    }
+    
+    render() {
+        const { searchReplace } = this.props;
+        const { regex } = this.state;
+        const invert = (prevState) => this.setState({ regex: !regex });
 
-                searchReplace(find.value, replace.value, {
-                    caseInsensitive: caseInsensitive.value,
-                    regex: regex.value,
-                    replaceAll: replaceAll.value
-                });
-            }
-            }>
-                <span className="input-wrapper">
-                    <label><em>Find:</em></label>
-                    <Input type="text" name="find" id="find" />
-                </span>
-                <span className="input-wrapper">
-                    <label><em>Replace:</em></label>
-                    <Input id="replace" name="replace" />
-                </span>
+        return (
+            <div>
+                <form onSubmit={(event) => {
+                    event.preventDefault();
 
-                <br />
+                    const { find, replace, regex, replaceAll, caseInsensitive } = event.target.elements;
 
-                <Button type="submit" variant="contained" color="primary" size="small">Apply</Button>
+                    searchReplace(find.value, replace.value, {
+                        caseInsensitive: caseInsensitive.value,
+                        regex: regex.value,
+                        replaceAll: replaceAll.value
+                    });
+                }
+                }>
+                    <span className="input-wrapper">
+                        <label><em>Find:</em></label>
+                        <Input type="text" name="find" id="find" />
+                    </span>
+                    <span className="input-wrapper">
+                        <label><em>Replace:</em></label>
+                        <Input id="replace" name="replace" />
+                    </span>
 
-                <span>
-                    <label>Regex</label>
-                    <Checkbox
-                        id="regex" name="regex"
-                        onClick={() => regex = !regex}
-                    />
-                </span>
+                    <br />
 
-                <span style={{ display: !regex ? "none" : "" }}>
-                    <label>Case-insensitive</label>
-                    <Checkbox
-                        id="caseInsensitive" name="caseInsensitive"
-                        disabled={!regex}
-                    />
-                </span>
+                    <Button type="submit" variant="contained" color="primary" size="small">Apply</Button>
 
-                <span style={{ display: !regex ? "none" : "" }}>
-                    <label>Replace all</label>
-                    <Checkbox
-                        id="replaceAll" name="replaceAll"
-                        disabled={!regex}
-                    />
-                </span>
-            </form>
-        </div>
-    );
+                    <span>
+                        <label>Regex</label>
+                        <Checkbox
+                            id="regex" name="regex"
+                            onClick={invert}
+                        />
+                    </span>
+
+                    <span style={{ display: !regex ? "none" : "" }}>
+                        <label>Case-insensitive</label>
+                        <Checkbox
+                            id="caseInsensitive" name="caseInsensitive"
+                            disabled={!regex}
+                        />
+                    </span>
+
+                    <span style={{ display: !regex ? "none" : "" }}>
+                        <label>Replace all</label>
+                        <Checkbox
+                            id="replaceAll" name="replaceAll"
+                            disabled={!regex}
+                        />
+                    </span>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default SearchAndReplace;
